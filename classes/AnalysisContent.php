dabb77da5b1e13a1b8e15ce08a68b529e3630940
PERALE <perale@rarus.ru>
2019-05-24 10:33:12 +0300

    22866 - Добавлен комментарий с преобразованием даты в timestamp

--- classes/AnalysisContent.php
+++ classes/AnalysisContent.php
@@ -33,6 +33,8 @@ class AnalysisContent
             '*.GIF',
             '*.ico',
             '*.ICO',
+            '*.pdf',
+            '*.PDF'
         ];
         $finder = new Finder();
         $finder->files()->in($dir . $linkToFolder)->notName($imgMaskName);
@@ -59,11 +61,32 @@ class AnalysisContent
             # возвращаем данные только с нужными тегами
 //            $tags = strip_tags($html, '<p><h1><h2><h3><h4><h5><span><ul><ol><li><img>');
             $tags = strip_tags($html, '<p><h1><h2><h3><h4><h5><span>');
-            $tags = str_replace([chr(13), chr(10)],'',$tags); //убираем табуляцию и переносы
+            $tags = str_replace([chr(13), chr(10)],'', $tags); //убираем табуляцию и переносы
 
 
             $fileSystem = new Filesystem();
             $fileSystem->dumpFile($dir . '/archive_edit/' . $link, $tags);
         }
     }
+
+
+
+
+
+
+
+    /*
+      #region date
+            // получили название папки с датой формата "20151117145645"
+            preg_match('#(\d{14}.*?)#m', $link, $date);
+
+            // преобразовали формат для преобразования в timestamp
+            $date = strptime($date[1], '%Y%m%d');
+            // преобразовали в timestamp
+            $timestamp = mktime(0, 0, 0, $date['tm_mon']+1, $date['tm_mday'], $date['tm_year']+1900);
+            // проверили правильность преобразования
+            $date = date('d.m.Y', $timestamp);
+            #endregion
+
+     * */
 }
